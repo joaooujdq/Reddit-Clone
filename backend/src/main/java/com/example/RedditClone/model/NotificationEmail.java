0bfd303c77d3d6c692864e29c2b62c0a380e8022
João Marcelo <49964202+joaooujdq@users.noreply.github.com>
2021-10-18 19:16:10 -0300

    Criando as entidades e suas interfaces

---
+++ backend/src/main/java/com/example/RedditClone/model/NotificationEmail.java
@@ -0,0 +1,4 @@
+package com.example.RedditClone.model;
+
+public class NotificationEmail {
+}
---
+++ backend/src/main/java/com/example/RedditClone/model/User.java
@@ -0,0 +1,40 @@
+package com.example.RedditClone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import java.time.Instant;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long userId;
+
+    @NotBlank(message = "Nome de usuário não preenchido")
+    private String username;
+
+    @NotBlank(message = "Senha não preenchida")
+    private String password;
+
+    @Email
+    @NotEmpty(message = "Email não preenchido")
+    private String email;
+    private Instant created;
+    private boolean enabled;
+
+}
