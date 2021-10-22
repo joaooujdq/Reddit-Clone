6eed09f0fe81ef5b584a3e21520ece6762c95849
João Marcelo <49964202+joaooujdq@users.noreply.github.com>
2021-10-22 09:07:28 -0300

    finalizando o frontend, ainda há alguns erros pendentes

---
+++ frontend/angular-reddit-clone/e2e/src/app.po.ts
@@ -0,0 +1,13 @@
+1 lines (9 sloc)  262 Bytes
+   
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
+  }
+
+  getTitleText() {
+    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  }
+}
